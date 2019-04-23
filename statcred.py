67c246e446efa3f3e091a9deb5f28ab4097318ae
Phil Garner <pgarner@gmx.ch>
2019-04-23 09:52:30 +0200

    More robust command line; more options

--- statcred.py
+++ statcred.py
@@ -11,21 +11,27 @@
 from scipy.special import betainc
 from scipy.special import betaincinv
 
-def betacred(k, n, j):
-    p = 1
+def betacred(k, n, j, p):
+    r = (1.0-p/100)/2
+    a = 1
     if j:
-        p = 0.5
-    l = betaincinv(k+p, n-k+p, 0.025)
-    u = betaincinv(k+p, n-k+p, 1.0-0.025)
+        a = 0.5
+    l = betaincinv(k+a, n-k+a, r)
+    u = betaincinv(k+a, n-k+a, 1.0-r)
     return (l, u)
-    
+
+
+
 import argparse
 ap = argparse.ArgumentParser("statcred")
-ap.add_argument("-k", type=int, help="number of successes")
-ap.add_argument("-n", type=int, help="number of trials")
+ap.add_argument("-k", type=int, default=0, help="number of successes")
+ap.add_argument("-n", type=int, default=0, help="number of trials")
 ap.add_argument("-j", action="store_true", help="use the Jeffreys interval")
+ap.add_argument("-p", type=float, default=95, help="pertinent percentage")
 arg = ap.parse_args()
 
-(l, u) = betacred(arg.k, arg.n, arg.j)
+(l, u) = betacred(arg.k, arg.n, arg.j, arg.p)
 
-print("95% limits are: {0:1.3f} {1:1.3f}".format(l, u))
+print("{0:.1f}% limits: {1:1.3f} {2:1.3f}".format(arg.p, l, u))
+
+# print(arg)
