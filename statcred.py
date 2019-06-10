c66a0dc44f000aeb7e420e830120c4528dffc1e3
Phil Garner <pgarner@gmx.ch>
2019-06-10 21:46:34 +0200

    Add t-test and example data files

--- statcred.py
+++ statcred.py
@@ -8,10 +8,16 @@
 #   Phil Garner, April 2019
 #
 
+import numpy as np
+from scipy.stats import ttest_ind
 from scipy.special import betainc
 from scipy.special import betaincinv
 
 def betacred(k, n, j, p):
+    """
+    Returns the upper and lower bounds of the credible interval based on a beta
+    distribution.
+    """
     r = (1.0-p/100)/2
     a = 1
     if j:
@@ -21,17 +27,29 @@ def betacred(k, n, j, p):
     return (l, u)
 
 
-
 import argparse
 ap = argparse.ArgumentParser("statcred")
+ap.add_argument("-t", choices=["beta", "t", "z"], default="beta",
+                help="the type of test to do")
 ap.add_argument("-k", type=int, default=0, help="number of successes")
 ap.add_argument("-n", type=int, default=0, help="number of trials")
 ap.add_argument("-j", action="store_true", help="use the Jeffreys interval")
 ap.add_argument("-p", type=float, default=95, help="pertinent percentage")
+ap.add_argument("-f", nargs="+",
+                help="data file(s) to be read using numpy.loadtxt")
 arg = ap.parse_args()
 
-(l, u) = betacred(arg.k, arg.n, arg.j, arg.p)
+if arg.t == "beta":
+    (l, u) = betacred(arg.k, arg.n, arg.j, arg.p)
+    print("{0:.1f}% limits: {1:1.3f} {2:1.3f}".format(arg.p, l, u))
 
-print("{0:.1f}% limits: {1:1.3f} {2:1.3f}".format(arg.p, l, u))
+elif arg.t == "t":
+    f = []
+    for i in range(len(arg.f)):
+        f.append(np.loadtxt(arg.f[i]))
+    if (len(f) != 2):
+        raise Exception("Must be two files for a t-test")
 
-# print(arg)
+    # Two-sample equal-variance two-tailed t-test
+    (s, p) = ttest_ind(f[0], f[1])
+    print("p = {0:.3f}".format(p))
